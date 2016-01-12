77ce110b6586122ae49bd9b8c2b78f2ff3961490
Thaís <tabr@cin.ufpe.br>
2016-01-12 19:26:44 -0200

    Code comment.

--- src/main/resources/Logback.groovy
+++ src/main/resources/Logback.groovy
@@ -4,6 +4,8 @@ import ch.qos.logback.core.FileAppender
 
 import static ch.qos.logback.classic.Level.INFO
 
+//Linux: change file name to lower case
+
 appender("STDOUT", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%d{HH:mm:ss} [%thread] %-5level %logger{36} %file:%line - %msg%n"

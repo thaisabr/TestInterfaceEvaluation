3f56b97f72904b34f2a17ff1e730bc29b02c07c1
Thaís <tabr@cin.ufpe.br>
2016-01-11 23:20:26 -0200

    Logging configuration

---
+++ src/main/resources/Logback.groovy
@@ -0,0 +1,19 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.FileAppender
+
+import static ch.qos.logback.classic.Level.INFO
+
+appender("STDOUT", ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{HH:mm:ss} [%thread] %-5level %logger{36} %file:%line - %msg%n"
+    }
+}
+appender("FILE", FileAppender) {
+    file = "output${File.separator}evaluation.log"
+    append = false
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{HH:mm:ss} [%thread] %-5level %logger{36} %file:%line - %msg%n"
+    }
+}
+root(INFO, ["FILE", "STDOUT"])

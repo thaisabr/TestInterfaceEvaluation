049bc311debc9cb438bff3c3c13e1ac86f4ab43e
Thaís <tabr@cin.ufpe.br>
2016-03-21 21:16:43 -0300

    Refactoring.

--- src/main/groovy/util/Util.groovy
+++ src/main/groovy/util/Util.groovy
@@ -9,45 +9,44 @@ import java.util.regex.Matcher
 
 class Util {
 
-    static final FILE_SEPARATOR_REGEX = /(\\|\/)/
-    static final NEW_LINE_REGEX = /\r\n|\n/
+    protected static final FILE_SEPARATOR_REGEX = /(\\|\/)/
+    protected static final NEW_LINE_REGEX = /\r\n|\n/
 
-    static final String NON_PRIMITIVE_ARRAY_PREFIX = "[L"
-    static final String FEATURE_FILENAME_EXTENSION = ".feature"
-    static final String JAR_FILENAME_EXTENSION = ".jar"
-    static final String GROOVY_EXTENSION = ".groovy"
-    static final String JAVA_EXTENSION = ".java"
-    static final String RUBY_EXTENSION = ".rb"
+    protected static final String NON_PRIMITIVE_ARRAY_PREFIX = "[L"
+    protected static final String FEATURE_FILENAME_EXTENSION = ".feature"
+    protected static final String JAR_FILENAME_EXTENSION = ".jar"
+    protected static final String GROOVY_EXTENSION = ".groovy"
+    protected static final String JAVA_EXTENSION = ".java"
+    protected static final String RUBY_EXTENSION = ".rb"
 
-    static final String GIT_EXTENSION = ".git"
-    static final String GITHUB_URL = "https://github.com/";
+    protected static final String GIT_EXTENSION = ".git"
+    protected static final String GITHUB_URL = "https://github.com/";
 
-    static final String REPOSITORY_FOLDER_PATH
-    static final String TASKS_FILE
-    static final String DEFAULT_TASK_FILE = "tasks.csv"
-    static final String DEFAULT_EVALUATION_FOLDER = "output"
-    static final String DEFAULT_EVALUATION_FILE = "$DEFAULT_EVALUATION_FOLDER${File.separator}evaluation_result.csv"
-    static final String GHERKIN_FILES_RELATIVE_PATH
-    static final String STEPS_FILES_RELATIVE_PATH
-    static final String UNIT_TEST_FILES_RELATIVE_PATH
-    static final String PRODUCTION_FILES_RELATIVE_PATH
-    static final String VIEWS_FILES_RELATIVE_PATH
+    protected static final String REPOSITORY_FOLDER_PATH
+    protected static final String TASKS_FILE
+    protected static final String DEFAULT_TASK_FILE = "tasks.csv"
+    protected static final String DEFAULT_EVALUATION_FOLDER = "output"
+    protected static final String DEFAULT_EVALUATION_FILE = "$DEFAULT_EVALUATION_FOLDER${File.separator}evaluation_result.csv"
+    protected static final String GHERKIN_FILES_RELATIVE_PATH
+    protected static final String STEPS_FILES_RELATIVE_PATH
+    protected static final String UNIT_TEST_FILES_RELATIVE_PATH
+    protected static final String PRODUCTION_FILES_RELATIVE_PATH
+    protected static final String VIEWS_FILES_RELATIVE_PATH
+    protected static final String VALID_EXTENSION
+    protected static final List<String> VALID_VIEW_FILES
 
-    static final List<String> STEP_KEYWORDS = new GherkinDialectProvider().defaultDialect.stepKeywords.unique()*.trim()
-    static final List<String> STEP_KEYWORDS_PT = new GherkinDialectProvider().getDialect("pt",null).stepKeywords.unique()*.trim()
+    protected static final List<String> STEP_KEYWORDS = new GherkinDialectProvider().defaultDialect.stepKeywords.unique()*.trim()
+    protected static final List<String> STEP_KEYWORDS_PT = new GherkinDialectProvider().getDialect("pt",null).stepKeywords.unique()*.trim()
 
-    //usado apenas em groovy
-    static final List<String> PAGE_METHODS = ["to", "at"]
+    protected static final LanguageOption CODE_LANGUAGE
 
-    static final LanguageOption CODE_LANGUAGE
+    protected static final String PROPERTIES_FILE_NAME = "configuration.properties"
+    protected static final Properties properties
 
-    static final String PROPERTIES_FILE_NAME = "configuration.properties"
-    static final Properties properties
-
-    static List<String> excludedPath
-    static List<String> excludedExtensions
-    static List<String> excludedFolders
-    private static regex_testCode
+    protected static List<String> excludedPath
+    protected static List<String> excludedExtensions
+    protected static List<String> excludedFolders
+    protected static regex_testCode
 
     static {
         properties = new Properties()
@@ -68,6 +67,21 @@ class Util {
                 Matcher.quoteReplacement(File.separator))
         VIEWS_FILES_RELATIVE_PATH = PRODUCTION_FILES_RELATIVE_PATH + File.separator + "views"
         CODE_LANGUAGE = (properties.'spgroup.language').trim().toUpperCase() as LanguageOption
+
+        switch(CODE_LANGUAGE){
+            case LanguageOption.RUBY:
+                VALID_EXTENSION = RUBY_EXTENSION
+                VALID_VIEW_FILES = [".html", ".html.haml", ".html.erb", ".html.slim"]
+                break
+            case LanguageOption.GROOVY:
+                VALID_EXTENSION = GROOVY_EXTENSION
+                VALID_VIEW_FILES = []
+                break
+            case LanguageOption.JAVA:
+                VALID_EXTENSION = JAVA_EXTENSION
+                VALID_VIEW_FILES = []
+                break
+        }
     }
 
     private static loadProperties(){
@@ -138,7 +152,8 @@ class Util {
      */
     static boolean isProductionCode(String path){
         if(path == null || path == "") false
-        else if( !(excludedPath).any{ path.contains(it)} && path.contains(PRODUCTION_FILES_RELATIVE_PATH+File.separator) ) true
+        else if( (VALID_VIEW_FILES+VALID_EXTENSION).any{ path.endsWith(it)} &&
+                path ==~ /.*$PRODUCTION_FILES_RELATIVE_PATH$FILE_SEPARATOR_REGEX.*/ ) true
         else false
     }
 
@@ -148,10 +163,10 @@ class Util {
      * @param files a list of file paths
      * @return a list of production file paths
      */
-    static List<String> findAllProductionFiles(List<String> files){
+    static Collection<String> findAllProductionFiles(Collection<String> files){
         files?.findAll{ file ->
-            !(excludedPath).any{ file.contains(it)} && file.contains(PRODUCTION_FILES_RELATIVE_PATH+File.separator)
-            //!(excludedPath).any{ file.contains(it)} && !isTestCode(file)
+            (VALID_VIEW_FILES+VALID_EXTENSION).any{file.endsWith(it)} &&
+                    file ==~ /.*$PRODUCTION_FILES_RELATIVE_PATH$FILE_SEPARATOR_REGEX.*/
         }
     }
 
@@ -163,8 +178,8 @@ class Util {
      */
     static List<CodeChange> findAllProductionFilesFromCodeChanges(List<CodeChange> changes){
         changes?.findAll{ change ->
-            !(excludedPath).any{ change.filename.contains(it)} && change.filename.contains(PRODUCTION_FILES_RELATIVE_PATH+File.separator)
-            //!(excludedPath).any{ change.filename.contains(it)} && !isTestCode(change.filename)
+            (VALID_VIEW_FILES+VALID_EXTENSION).any{ change.filename.endsWith(it)} &&
+                    change.filename ==~ /.*$PRODUCTION_FILES_RELATIVE_PATH$FILE_SEPARATOR_REGEX.*/
         }
     }
 
@@ -276,13 +291,11 @@ class Util {
 
         f?.eachDirRecurse{ dir ->
             dir.listFiles().each{
-                if(it.isFile()){
-                    files += it.absolutePath.replaceAll(FILE_SEPARATOR_REGEX, Matcher.quoteReplacement(File.separator))
-                }
+                if(it.isFile()) files += it.absolutePath.replaceAll(FILE_SEPARATOR_REGEX, Matcher.quoteReplacement(File.separator))
             }
         }
         f?.eachFile{
-            files += it.absolutePath.replaceAll(FILE_SEPARATOR_REGEX, Matcher.quoteReplacement(File.separator))
+            if(it.isFile()) files += it.absolutePath.replaceAll(FILE_SEPARATOR_REGEX, Matcher.quoteReplacement(File.separator))
         }
         files
     }
@@ -298,140 +311,15 @@ class Util {
         camelCase.replaceAll(/(\B[A-Z])/,'_$1').toLowerCase()
     }
 
-    static String getClassName(String path){
-        if(!path || path.empty || path.isAllWhitespace()) return ""
-        def firstIndex = path.lastIndexOf(File.separator)
-        def lastIndex = path.lastIndexOf(".")
-        def underscore = ""
-        if(firstIndex>=0 && lastIndex>=0) underscore = path.substring(firstIndex+1,lastIndex)
-        underscoreToCamelCase(underscore)
-    }
-
-    /********** A PARTIR DAQUI, TUDO FOI INCLUÍDO APÓS INTEGRAÇÃO COM ANALISADOR DE CÓDIGO GROOVY. *******************/
-
     static findJarFilesFromDirectory(String directory){
         def files = findFilesFromDirectory(directory)
         files.findAll{it.contains(JAR_FILENAME_EXTENSION)}
     }
 
-    static configClassnameFromMethod(String className){
-        if (className.startsWith(NON_PRIMITIVE_ARRAY_PREFIX) && className.endsWith(";")) {
-            className = className.substring(NON_PRIMITIVE_ARRAY_PREFIX.length(), className.length() - 1)
-        }
-        return className
-    }
-
-    static boolean isPageMethod(String referencedMethod){
-        if(referencedMethod in PAGE_METHODS) true
-        else false
-    }
-
-    /***
-     *
-     * @param className
-     * @param extension
-     * @param projectFiles
-     * @return
-     */
     private static getClassPath(String className, String extension, Collection<String> projectFiles){
         def name = ClassUtils.convertClassNameToResourcePath(className)+extension
         name = name.replaceAll(FILE_SEPARATOR_REGEX, Matcher.quoteReplacement(File.separator))
         projectFiles?.find{ it.endsWith(File.separator+name) }
-    }
-
-    /***
-     * Provides the file path of a Groovy class.
-     *
-     * @param className class full name
-     * @param projectFiles list of file names
-     * @return the file name that matches to the class name
-     */
-    static String getClassPathForGroovy(String className, Collection<String> projectFiles){
-        getClassPath(className, GROOVY_EXTENSION, projectFiles)
-    }
-
-    /***
-     * Provides the file path of a Java class.
-     *
-     * @param className class full name
-     * @param projectFiles list of file names
-     * @return the file name that matches to the class name
-     */
-    static String getClassPathForJava(String className, Collection<String> projectFiles){
-        getClassPath(className, JAVA_EXTENSION, projectFiles)
-    }
-
-    /***
-     * Provides the file path of a Ruby class or module.
-     *
-     * @param className class or module short name
-     * @param projectFiles list of file names
-     * @return the file name that matches to the class or module name
-     */
-    static String getClassPathForRuby(String className, Collection<String> projectFiles){
-        def underscore = camelCaseToUnderscore(className)
-        def name = (underscore+RUBY_EXTENSION).replaceAll(FILE_SEPARATOR_REGEX, Matcher.quoteReplacement(File.separator))
-        def regex_expression = ".*${Matcher.quoteReplacement(File.separator)}$name"
-        projectFiles?.find{ it ==~ /$regex_expression/ }
-    }
-
-    /**
-     * (A FAZER; DEFINIR REGRA DE MAPEAMENTO) ISSO É ESPECÍFICO PARA RUBY
-     * @param resourcePath pode ser url, diretório local ou arquivo local
-     * @param projectFiles
-     * @return
-     */
-    static String findViewPathForRailsProjects(String resourcePath, List projectFiles){
-        def name = resourcePath.replaceAll(FILE_SEPARATOR_REGEX, Matcher.quoteReplacement(File.separator))
-        int n = name.count(File.separator)
-        if(n>1){
-            def index = name.lastIndexOf(File.separator)
-            name = name.substring(0,index)
-        }
-        def match = projectFiles?.find{ it.contains(name) }
-        if(match) name = match
-        else name = ""
-        name
-    }
-
-    /**
-     * ISSO É ESPECÍFICO PARA GROOVY
-     */
-    static String findViewPathForGrailsProjects(String resourcePath, List projectFiles){
-        def name = resourcePath.replaceAll(FILE_SEPARATOR_REGEX, Matcher.quoteReplacement(File.separator))
-        int n = name.count(File.separator)
-        if(n>1){
-            def index = name.lastIndexOf(File.separator)
-            name = name.substring(0,index)
-        }
-        def match = projectFiles?.find{ it.contains(name) }
-        if(match) name = match
-        else name = ""
-        name
-    }
-
-    /**
-     * ISSO É ESPECÍFICO PARA GROOVY
-     */
-    static boolean isValidClassByAPI(String referencedClass){
-        def INVALID_CLASS_REGEX = /.*(groovy|java|springframework|apache|grails|spock|geb|selenium|cucumber).*/
-        if(INVALID_CLASS_REGEX) {
-            if(referencedClass ==~ INVALID_CLASS_REGEX) false
-            else true
-        }
-        else true
-    }
-
-    /**
-     * ISSO É ESPECÍFICO PARA GROOVY
-     * Filters method calls to ignore methods provided by API.
-     * @param referencedClass
-     * @param path
-     * @return true if is a valid class. Otherwise, returns false.
-     */
-    static boolean isValidClass(String referencedClass, String path){
-        if(path!=null && !path.isEmpty() && isValidClassByAPI(referencedClass)) true
-        else false
     }
 
 }
---
+++ src/main/groovy/util/ruby/RubyUtil.groovy
@@ -0,0 +1,76 @@
+package util.ruby
+
+import util.Util
+
+import java.util.regex.Matcher
+
+class RubyUtil extends Util {
+
+    static final String ROUTES_FILE = File.separator+"config"+File.separator+"routes.rb"
+    static final String ROUTES_ID = "ROUTES"
+    static final String ROUTE_SUFIX = "_path"
+
+    /***
+     * Provides the file path of a Ruby class or module.
+     *
+     * @param className class or module short name
+     * @param projectFiles list of file names
+     * @return the file name that matches to the class or module name
+     */
+    static String getClassPathForRubyClass(String className, Collection<String> projectFiles){
+        def underscore = camelCaseToUnderscore(className)
+        def name = (underscore+RUBY_EXTENSION).replaceAll(FILE_SEPARATOR_REGEX, Matcher.quoteReplacement(File.separator))
+        def regex_expression = ".*${Matcher.quoteReplacement(File.separator)}$name"
+        projectFiles?.find{ it ==~ /$regex_expression/ }
+    }
+
+    static String getClassPathForRubyInstanceVariable(String varName, Collection<String> projectFiles){
+        def name = varName+RUBY_EXTENSION
+        def regex_expression = ".*${Matcher.quoteReplacement(File.separator)}$name"
+        projectFiles?.find{ it ==~ /$regex_expression/ }
+    }
+
+    static List<String> findViewPathForRailsProjects(String resourcePath, List projectFiles){
+        if(!resourcePath || resourcePath.empty || resourcePath.allWhitespace ) return ""
+
+        def result = []
+        def viewFiles = projectFiles?.findAll{ file ->
+            file.contains(VIEWS_FILES_RELATIVE_PATH) && VALID_VIEW_FILES.any{ file.endsWith(it) }
+        }
+        def name = resourcePath.replaceAll(FILE_SEPARATOR_REGEX, Matcher.quoteReplacement(File.separator))
+
+        if(name.contains(".")){ //is a file
+            def match = viewFiles?.findAll{ it.contains(name) }
+            if(match) result += match
+        } else if(name.contains("#")){
+            def index = name.indexOf("#")
+            def controller = name.substring(0, index)
+            def action = name.substring(index+1,name.length())
+            if(controller && action){
+                def match = viewFiles?.findAll{ it.contains(File.separator+controller+File.separator+action)}
+                if(match && !match.empty) result += match
+            }
+        } else { //is a directory
+            def matches = viewFiles?.findAll{ it.contains(name) }
+            if(matches && matches.size()>0){
+                if(matches.size() == 1) result = matches
+                else {
+                    def match = matches.find{ it.contains("index") }
+                    if(match) result += match
+                    else result = matches
+                }
+            }
+        }
+        result
+    }
+
+    static String getClassName(String path){
+        if(!path || path.empty || path.isAllWhitespace()) return ""
+        def firstIndex = path.lastIndexOf(File.separator)
+        def lastIndex = path.lastIndexOf(".")
+        def underscore = ""
+        if(firstIndex>=0 && lastIndex>=0) underscore = path.substring(firstIndex+1,lastIndex)
+        underscoreToCamelCase(underscore)
+    }
+
+}

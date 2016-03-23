61f31e24dd438b3b5fc36334808e57648ff82ad6
Thaís <tabr@cin.ufpe.br>
2016-03-22 21:58:10 -0300

    Fixing encoding.

--- src/main/groovy/testCodeAnalyser/ruby/RubyStepRegexVisitor.groovy
+++ src/main/groovy/testCodeAnalyser/ruby/RubyStepRegexVisitor.groovy
@@ -4,6 +4,8 @@ import org.jrubyparser.ast.RegexpNode
 import org.jrubyparser.util.NoopVisitor
 import testCodeAnalyser.StepRegex
 
+import java.nio.charset.StandardCharsets
+
 /***
  * Visits step definition files looking for regex expressions. The regex is used to match steps in Gherkin files and
  * step definitions.
@@ -21,7 +23,8 @@ class RubyStepRegexVisitor extends NoopVisitor {
     @Override
     Object visitRegexpNode(RegexpNode iVisited) {
         super.visitRegexpNode(iVisited)
-        regexs += new StepRegex(path: path, value:iVisited.value, line: iVisited.position.startLine)
+        regexs += new StepRegex(path: path, value:new String(iVisited.value.getBytes(), StandardCharsets.UTF_8),
+                line: iVisited.position.startLine)
     }
 
 }

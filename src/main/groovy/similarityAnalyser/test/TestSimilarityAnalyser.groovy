5ebb64ee2b7e38294d25b6f965cf993c6acb8421
Thaís <tabr@cin.ufpe.br>
2016-04-13 13:17:01 -0300

    Computing similarity between test interfaces by Jaccard index and cosine similarity.

--- src/main/groovy/similarityAnalyser/test/TestSimilarityAnalyser.groovy
+++ src/main/groovy/similarityAnalyser/test/TestSimilarityAnalyser.groovy
@@ -1,11 +1,9 @@
 package similarityAnalyser.test
 
-import groovy.util.logging.Slf4j
 import org.apache.commons.math3.linear.ArrayRealVector
 import org.apache.commons.math3.linear.RealVector
 import taskAnalyser.TaskInterface
 
-@Slf4j
 class TestSimilarityAnalyser {
 
     private static RealVector toRealVector(Collection terms, Collection set) {
@@ -18,23 +16,23 @@ class TestSimilarityAnalyser {
         vector
     }
 
-    static calculateSimilarityByCosine(Collection set1, Collection set2){
-        if(!set1 || set1.empty || !set2 || set2.empty) return 0
+    private static double getCosineSimilarity(RealVector v1, RealVector v2){
+        v1.dotProduct(v2) / (v1.norm * v2.norm)
+    }
+
+    static double calculateSimilarityByCosine(Collection set1, Collection set2){
+        if(!set1 || set1.empty || !set2 || set2.empty) return 0.0
         def terms = (set1 + set2).sort()
-
         RealVector v1 = toRealVector(terms, set1.sort())
-        log.info "vector1 (binary): $v1"
         RealVector v2 = toRealVector(terms, set2.sort())
-        log.info "vector2 (binary): $v2"
-
         getCosineSimilarity(v1, v2)
     }
 
-    static calculateSimilarityByJaccard(Collection set1, Collection set2){
-        if(!set1 || !set2 || set1.isEmpty() || set2.isEmpty()) 0
+    static double calculateSimilarityByJaccard(Collection set1, Collection set2){
+        if(!set1 || !set2 || set1.isEmpty() || set2.isEmpty()) return 0.0
         def intersection = set1.intersect(set2).size()
         def divisor = set1.size()+set2.size()-intersection
-        if(divisor==0) 0
+        if(divisor==0) 0.0
         else intersection/divisor
     }
 
@@ -44,10 +42,6 @@ class TestSimilarityAnalyser {
 
     static calculateSimilarityByCosine(TaskInterface interface1, TaskInterface interface2){
         calculateSimilarityByCosine(interface1.findAllFiles(), interface2.findAllFiles())
-    }
-
-    private static double getCosineSimilarity(RealVector v1, RealVector v2){
-        v1.dotProduct(v2) / (v1.norm * v2.norm)
     }
 
 }

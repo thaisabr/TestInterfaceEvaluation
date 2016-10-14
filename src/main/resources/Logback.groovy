c7ec8bc42bae3602ca894d3433020c8669b74f31
Thaís Burity <tabr@cin.ufpe.br>
2016-10-14 14:02:08 -0300

    Refactoring.

--- src/main/resources/Logback.groovy
+++ src/main/resources/Logback.groovy
@@ -1,6 +1,7 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.core.FileAppender
+import util.ConstantData
 
 import static ch.qos.logback.classic.Level.INFO
 
@@ -12,7 +13,7 @@ appender("STDOUT", ConsoleAppender) {
     }
 }
 appender("FILE", FileAppender) {
-    file = "output${File.separator}evaluation.log"
+    file = "${ConstantData.DEFAULT_EVALUATION_FOLDER}${File.separator}evaluation.log"
     append = false
     encoder(PatternLayoutEncoder) {
         pattern = "%d{HH:mm:ss} [%thread] %-5level %logger{36} %file:%line - %msg%n"

28c57b6451314bedc0160ad52e04fb4fe3c0e84b
Thaís Burity <tabr@cin.ufpe.br>
2016-10-28 15:14:13 -0300

    Fixing route configuration.

--- src/main/groovy/testCodeAnalyser/ruby/routes/RubyNonResourcefulPropertiesVisitor.groovy
+++ src/main/groovy/testCodeAnalyser/ruby/routes/RubyNonResourcefulPropertiesVisitor.groovy
@@ -3,7 +3,6 @@ package testCodeAnalyser.ruby.routes
 import org.jrubyparser.ast.StrNode
 import org.jrubyparser.ast.SymbolNode
 import org.jrubyparser.util.NoopVisitor
-import util.RegexUtil
 
 
 class RubyNonResourcefulPropertiesVisitor extends NoopVisitor {
@@ -33,47 +32,58 @@ class RubyNonResourcefulPropertiesVisitor extends NoopVisitor {
         def controllerActionString = null
         def result //name, value, method
 
-        if (values.size() < 2) return
+        //solar example (https://github.com/wwagner33/solar/blob/b626d6c4a43ced0a53bf4f71d9ec6093bcad5c50/config/routes.rb
+        if (values.size() < 2) {
+            pathValue = values.get(0).value
+            def i = pathValue.indexOf("/")
+            if(i<0) return
+            String controller = pathValue.substring(0,i)
+            String action = pathValue.substring(i+1)
+            controllerActionString = "$controller#$action"
+            if (!pathValue.startsWith("/")) pathValue = "/" + pathValue
+            pathMethodName = pathValue.substring(1).replaceAll("/", "_")
+            pathValue += "(/.*)?"
+        } else{
+            //relevant nodes: to, as, controller, action, via, defaults, constraints
+            //TO REMEMBER: via, defaults e contraints sao conjuntos de valores sem relevancia nesse ponto e por isso nao sao extraidos
+            def indexTo = values.findIndexOf { it.value == "to" }
+            def indexAs = values.findIndexOf { it.value == "as" }
+            def indexController = values.findIndexOf { it.value == "controller" }
+            def indexAction = values.findIndexOf { it.value == "action" }
+            def indexPath = 0
 
-        //relevant nodes: to, as, controller, action, via, defaults, constraints
-        //TO REMEMBER: via, defaults e contraints sao conjuntos de valores sem relevancia nesse ponto e por isso nao sao extraidos
-        def indexTo = values.findIndexOf { it.value == "to" }
-        def indexAs = values.findIndexOf { it.value == "as" }
-        def indexController = values.findIndexOf { it.value == "controller" }
-        def indexAction = values.findIndexOf { it.value == "action" }
-        def indexPath = 0
+            //there is no explicit name
+            if (indexTo == 0 || indexController == 0) indexPath = -1
 
-        //there is no explicit name
-        if (indexTo == 0 || indexController == 0) indexPath = -1
+            //there is explicit controller
+            if (indexController > -1) controllerValue = values.get(indexController + 1).value
 
-        //there is explicit controller
-        if (indexController > -1) controllerValue = values.get(indexController + 1).value
+            //there is explicit action
+            if (indexAction > -1) actionValue = values.get(indexAction + 1).value
 
-        //there is explicit action
-        if (indexAction > -1) actionValue = values.get(indexAction + 1).value
+            //there is explicit pathMethodName
+            if (indexAs > -1) pathMethodName = values.get(indexAs + 1).value
 
-        //there is explicit pathMethodName
-        if (indexAs > -1) pathMethodName = values.get(indexAs + 1).value
+            //there is explicit path value
+            if (indexPath > -1) pathValue = values.get(indexPath).value
+            pathValue = pathValue.replaceAll("/:(\\w|\\.|:|#|&|=|\\+|\\?)*/", "/.*/").replaceAll("/:(\\w|\\.|:|#|&|=|\\+|\\?)*[^/()]", "/.*")
+            if (!pathValue.startsWith("/")) pathValue = "/" + pathValue
+            if (pathMethodName.empty && pathValue.size() > 1 && !pathValue.contains(".*")) {
+                pathMethodName = pathValue.substring(1).replaceAll("/", "_")
+            }
 
-        //there is explicit path value
-        if (indexPath > -1) pathValue = values.get(indexPath).value
-        pathValue = pathValue.replaceAll("/:(\\w|\\.|:|#|&|=|\\+|\\?)*/", "/.*/").replaceAll("/:(\\w|\\.|:|#|&|=|\\+|\\?)*[^/()]", "/.*")
-        if (!pathValue.startsWith("/")) pathValue = "/" + pathValue
-        if (pathMethodName.empty && pathValue.size() > 1 && !pathValue.contains(".*")) {
-            pathMethodName = pathValue.substring(1).replaceAll(RegexUtil.FILE_SEPARATOR_REGEX, "_")
-        }
-
-        //configures controllerActionString value
-        if (controllerValue && actionValue) controllerActionString = "$controllerValue#$actionValue"
-        else if (controllerValue && !actionValue) controllerActionString = "$controllerValue#index"
-        else {
-            if (indexPath == 0 && indexTo == -1) controllerActionString = values.get(indexPath + 1).value
-            else if (indexTo > -1) controllerActionString = values.get(indexTo + 1).value
+            //configures controllerActionString value
+            if (controllerValue && actionValue) controllerActionString = "$controllerValue#$actionValue"
+            else if (controllerValue && !actionValue) controllerActionString = "$controllerValue#index"
+            else {
+                if (indexPath == 0 && indexTo == -1) controllerActionString = values.get(indexPath + 1).value
+                else if (indexTo > -1) controllerActionString = values.get(indexTo + 1).value
+            }
         }
 
         //configures result
         result = [name: pathMethodName, value: pathValue, arg: controllerActionString]
-        return result
+        result
     }
 
     @Override

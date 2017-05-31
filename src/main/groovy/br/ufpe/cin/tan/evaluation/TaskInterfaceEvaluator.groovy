03da41353aea7e489a4f54dba6e2d87ea3070a1b
Thaís Burity <tabr@cin.ufpe.br>
2017-05-31 17:27:18 -0300

    Refactoring.

--- src/main/groovy/br/ufpe/cin/tan/evaluation/TaskInterfaceEvaluator.groovy
+++ src/main/groovy/br/ufpe/cin/tan/evaluation/TaskInterfaceEvaluator.groovy
@@ -5,10 +5,6 @@ import br.ufpe.cin.tan.analysis.itask.ITest
 
 class TaskInterfaceEvaluator {
 
-    private static calculateTruePositives(Set set1, Set set2) {
-        (set1.intersect(set2)).size()
-    }
-
     /***
      * Calculates precision of test based task interface considering files only.
      *
@@ -18,18 +14,10 @@ class TaskInterfaceEvaluator {
      */
     static double calculateFilesPrecision(ITest iTest, IReal iReal) {
         double result = 0
-        if (!iTest || iTest.empty || !iReal || iReal.empty) return 0
+        if(invalidInput(iTest, iReal)) return result
         def testFiles = iTest.findFilteredFiles()
         def truePositives = calculateTruePositives(testFiles, iReal.findFilteredFiles())
         if (truePositives > 0) result = (double) truePositives / testFiles.size()
-        result
-    }
-
-    static double calculateFilesPrecision(Set iTest, Set iReal) {
-        double result = 0
-        if (!iTest || iTest.empty || !iReal || iReal.empty) return 0
-        def truePositives = calculateTruePositives(iTest, iReal)
-        if (truePositives > 0) result = (double) truePositives / iTest.size()
         result
     }
 
@@ -42,19 +30,36 @@ class TaskInterfaceEvaluator {
      */
     static double calculateFilesRecall(ITest iTest, IReal iReal) {
         double result = 0
-        if (!iTest || iTest.empty || !iReal || iReal.empty) return 0
+        if(invalidInput(iTest, iReal)) return result
         def realFiles = iReal.findFilteredFiles()
         def truePositives = calculateTruePositives(iTest.findFilteredFiles(), realFiles)
         if (truePositives > 0) result = (double) truePositives / realFiles.size()
         result
     }
 
+    static double calculateFilesPrecision(Set iTest, Set iReal) {
+        double result = 0
+        if(invalidInput(iTest, iReal)) return result
+        def truePositives = calculateTruePositives(iTest, iReal)
+        if (truePositives > 0) result = (double) truePositives / iTest.size()
+        result
+    }
+
     static double calculateFilesRecall(Set iTest, Set iReal) {
         double result = 0
-        if (!iTest || iTest.empty || !iReal || iReal.empty) return 0
+        if(invalidInput(iTest, iReal)) return result
         def truePositives = calculateTruePositives(iTest, iReal)
         if (truePositives > 0) result = (double) truePositives / iReal.size()
         result
+    }
+
+    private static calculateTruePositives(Set set1, Set set2) {
+        (set1.intersect(set2)).size()
+    }
+
+    private static invalidInput(iTest, iReal){
+        if (!iTest || iTest.empty || !iReal || iReal.empty) true
+        else false
     }
 
 }

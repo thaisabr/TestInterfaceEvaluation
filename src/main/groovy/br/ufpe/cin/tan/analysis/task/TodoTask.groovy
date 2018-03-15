4d947bc28936cc9842c82feb04eadc174b186cbb
thaisabr <tabr@cin.ufpe.br>
2018-03-15 09:25:08 -0300

    Refactoring.

--- src/main/groovy/br/ufpe/cin/tan/analysis/task/TodoTask.groovy
+++ src/main/groovy/br/ufpe/cin/tan/analysis/task/TodoTask.groovy
@@ -3,6 +3,7 @@ package br.ufpe.cin.tan.analysis.task
 import br.ufpe.cin.tan.analysis.itask.ITest
 import br.ufpe.cin.tan.commit.change.gherkin.ChangedGherkinFile
 import br.ufpe.cin.tan.exception.CloningRepositoryException
+import br.ufpe.cin.tan.util.RegexUtil
 import br.ufpe.cin.tan.util.Util
 import gherkin.AstBuilder
 import gherkin.Parser
@@ -12,6 +13,8 @@ import gherkin.ast.GherkinDocument
 import groovy.time.TimeCategory
 import groovy.time.TimeDuration
 import groovy.util.logging.Slf4j
+
+import java.util.regex.Matcher
 
 /***
  * Represents a new task, that is, a task that contains test code but the production code is not done yet. The task is
@@ -23,18 +26,15 @@ class TodoTask extends Task {
     List<ChangedGherkinFile> testDescription
 
     /***
-     *
      * @param rootDirectory repository path. It can be a URL or local folder.
-     * @param isRemote if true, means it is a remote repository (the first parameter is a URL)
      * @param id task code to identify it.
      * @param scenarios a list of map objects that identifies a Gherkin file and its scenarios that
      *        are related to the task, by keywords 'path' and 'lines' respectively.
      */
-    TodoTask(String rootDirectory, boolean isRemote, int id, scenarios) throws CloningRepositoryException {
+    TodoTask(String rootDirectory, int id, scenarios) throws CloningRepositoryException {
         super(rootDirectory, id)
         testCodeAnalyser.configureProperties()
-        if (isRemote) findAllRelatedGherkinFile(gitRepository.localPath, scenarios)
-        else findAllRelatedGherkinFile(rootDirectory, scenarios)
+        findAllRelatedGherkinFile(scenarios)
     }
 
     @Override
@@ -61,12 +61,21 @@ class TodoTask extends Task {
         testDescription
     }
 
-    private findAllRelatedGherkinFile(String rootDirectory, scenarios) {
+    private configureFeaturePath(String entry) {
+        String featureName = ""
+        if (entry.contains(Util.GHERKIN_FILES_RELATIVE_PATH)) {
+            def index = entry.indexOf(Util.GHERKIN_FILES_RELATIVE_PATH)
+            featureName = entry.substring(index).replaceAll(RegexUtil.FILE_SEPARATOR_REGEX, Matcher.quoteReplacement(File.separator))
+        }
+        testCodeAnalyser.repositoryPath + File.separator + featureName
+    }
+
+    private findAllRelatedGherkinFile(scenarios) {
         List<ChangedGherkinFile> gherkinFiles = []
         Parser<GherkinDocument> parser = new Parser<>(new AstBuilder())
         scenarios?.each { scenario ->
             try {
-                def path = rootDirectory + File.separator + Util.GHERKIN_FILES_RELATIVE_PATH + File.separator + scenario.path
+                def path = configureFeaturePath(scenario.path)
                 def reader = new FileReader(path)
                 Feature feature = parser.parse(reader)?.feature
                 reader.close()

7d90ed8c56e94db8734e9256c839e133d6173b05
tabr <tabr@cin.ufpe.br>
2023-09-05 16:46:57 -0300

    Fixing problem to read ruby file.

--- src/main/groovy/br/ufpe/cin/tan/test/ruby/views/ViewCodeExtractor.groovy
+++ src/main/groovy/br/ufpe/cin/tan/test/ruby/views/ViewCodeExtractor.groovy
@@ -2,8 +2,10 @@ package br.ufpe.cin.tan.test.ruby.views
 
 import br.ufpe.cin.tan.util.Util
 import br.ufpe.cin.tan.util.ruby.RubyConstantData
+import groovy.util.logging.Slf4j
 import org.jruby.embed.ScriptingContainer
 
+@Slf4j
 class ViewCodeExtractor {
 
     ScriptingContainer container
@@ -17,9 +19,20 @@ class ViewCodeExtractor {
         container.loadPaths.add(Util.GEM_PARSER)
         container.loadPaths.add(Util.GEM_AST)
 
+        String classpath = System.getProperty("java.class.path")
+        log.info "CLASSPATH: " + classpath
+
         ClassLoader loader = Thread.currentThread().getContextClassLoader()
         InputStream is = loader.getResourceAsStream(RubyConstantData.VIEW_ANALYSER_FILE)
-        receiver = container.runScriptlet(is, RubyConstantData.VIEW_ANALYSER_FILE)
+        //receiver = container.runScriptlet(is, RubyConstantData.VIEW_ANALYSER_FILE)
+        String code = """
+            require 'rubygems'
+            require 'lib/task_analyser'
+            
+            def extract_production_code(file_path)
+              TaskAnalyser.new.grab_controllers(file_path)
+            end"""
+        receiver = container.runScriptlet(code)
     }
 
     String extractCode(String path) {

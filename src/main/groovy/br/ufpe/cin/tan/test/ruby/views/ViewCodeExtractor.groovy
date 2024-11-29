4e5318fdfa5c978b5228925c63b24b06f84e885e
Alex Lopes <alex.lsilva154@gmail.com>
2024-11-29 10:23:05 -0300

    removido load desnecessarios após adcionado as gems no maven

--- src/main/groovy/br/ufpe/cin/tan/test/ruby/views/ViewCodeExtractor.groovy
+++ src/main/groovy/br/ufpe/cin/tan/test/ruby/views/ViewCodeExtractor.groovy
@@ -12,17 +12,11 @@ class ViewCodeExtractor {
 
     ViewCodeExtractor() {
         container = new ScriptingContainer()
-        container.loadPaths.add(Util.GEMS_PATH)
-        container.loadPaths.add(Util.GEM_INFLECTOR)
-        container.loadPaths.add(Util.GEM_I18N)
-        container.loadPaths.add(Util.GEM_PARSER)
-        container.loadPaths.add(Util.GEM_AST)
 
         String classpath = System.getProperty("java.class.path")
         log.info "CLASSPATH: " + classpath
 
         String code = """
-            require 'rubygems'
             require 'lib/task_analyser'
             
             def extract_production_code(file_path)
